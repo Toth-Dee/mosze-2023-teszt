22ce4c1df8f28e5505e5d2288889d0921b50e3ad
Toth-Dee <pingvinke51@gmail.com>
2023-09-24 12:21:30 +0200

    git-hazi

---
+++ main.cpp
@@ -0,0 +1,26 @@
+#include <iostream>
+
+constexpr int N_ELEMENTS = 100;
+
+int main()
+{
+    int *b = new int[NELEMENTS];
+    std::cout << '1-100 ertekek duplazasa'
+    for (int i = 0;)
+    {
+        b[i] = i * 2;
+    }
+    for (int i = 0; i; i++)
+    {
+        std::cout << "Ertek:"
+    }    
+    std::cout << "Atlag szamitasa: " << std::endl;
+    int atlag;
+    for (int i = 0; i < N_ELEMENTS, i++)
+    {
+        atlag += b[i]
+    }
+    atlag /= N_ELEMENTS;
+    std::cout << "Atlag: " << atlag << std::endl;
+    return 0;
+}
